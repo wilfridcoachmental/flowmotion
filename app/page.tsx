5d427723abcb11d205b215939ced85c67fec1780
wilfridcoachmental <wilfridcoachmental@gmail.com>
2026-04-23 14:26:27 +0200

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
 export default function Page() {
   return (
-    <main style={{
-      height: "100vh",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontFamily: "system-ui",
-      background: "#0f172a",
-      color: "white"
-    }}>
+    <main>
       <h1>Pôle Performance TT - Flowmotion</h1>
     </main>
-  );
+  )
 }
