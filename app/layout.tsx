c8ae3f0a574243f40539a901aeb9ef5e487316b7
wilfridcoachmental <wilfridcoachmental@gmail.com>
2026-04-23 14:26:54 +0200

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,7 @@
-export const metadata = {
-  title: "Flowmotion",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <body>{children}</body>
     </html>
-  );
+  )
 }
